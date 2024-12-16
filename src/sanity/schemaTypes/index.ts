28a4bec74626818e021a710655bd0140c10b536d
Your Name <you@example.com>
2024-12-16 06:27:39 +0500

    first commit

---
+++ src/sanity/schemaTypes/index.ts
@@ -0,0 +1,6 @@
+import { Product } from './product'
+import { type SchemaTypeDefinition } from 'sanity'
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [Product]
+}
---
+++ src/sanity/schemaTypes/product.ts
@@ -0,0 +1,32 @@
+export const Product ={
+    name: "product",
+    title: "Product",
+    type: "document",
+    fields:[
+        {
+            name:"name",
+            title:"Name",
+            type:"string"
+        },
+        {
+            name:"fullname",
+            title:"Full Name",
+            type:"string"
+        },
+        {
+            name: "heading",
+            title: "Heading",
+            type: "string"
+        },
+        {
+            name: "image",
+            title: "Blog Image",
+            type: "image"
+        },
+        {
+            name:"category",
+            title:"Category",
+            type:"string"        }
+    ]
+
+}
